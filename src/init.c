42b4b51598aa8bc764f87a29402db29dd39a0798
fwatanab <fwatanab@student.42tokyo.jp>
2023-10-31 03:01:05 +0900

    norm crear

--- src/init.c
+++ src/init.c
@@ -6,11 +6,25 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 00:15:40 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/10/27 19:31:35 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/10/31 01:44:59 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+t_token_check	*checker_init(void)
+{
+	t_token_check	*check;
+
+	check = (t_token_check *)malloc(sizeof(t_token_check));
+	if (!check)
+		malloc_error();
+	check->start = NULL;
+	check->token = NULL;
+	check->d_quote = false;
+	check->s_quote = false;
+	return (check);
+}
 
 t_node	*node_init(void)
 {
