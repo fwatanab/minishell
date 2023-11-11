00df1845eb88611f77c023c3bdfcf3245bb4c058
fwatanab <fwatanab@student.42tokyo.jp>
2023-11-11 17:58:51 +0900

    コマンドが1つの時のtypeの修正

--- src/parser_utils.c
+++ src/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:02:12 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/11/11 17:38:11 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/11/11 17:58:04 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,10 +73,12 @@ void	one_n_command(t_node *node)
 {
 		node->args = node->left->args;
 		node->name = node->left->name;
+		node->type = node->left->type;
 		if (node->left->redir)
 			node->redir = node->left->redir;
 		node->left->args = NULL;
 		node->left->name = NULL;
+		node->left->type = NONE;
 		node->left->redir = NULL;
 }
 
