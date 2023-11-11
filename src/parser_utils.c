05d404ebcb546467542911fb83127ee98501ce21
fwatanab <fwatanab@student.42tokyo.jp>
2023-11-11 17:40:50 +0900

    コマンドが1つの時redirがない時は触らないように修正

--- src/parser_utils.c
+++ src/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:02:12 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/11/08 20:04:07 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/11/11 17:38:11 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,8 @@ void	one_n_command(t_node *node)
 {
 		node->args = node->left->args;
 		node->name = node->left->name;
-		node->redir = node->left->redir;
+		if (node->left->redir)
+			node->redir = node->left->redir;
 		node->left->args = NULL;
 		node->left->name = NULL;
 		node->left->redir = NULL;
