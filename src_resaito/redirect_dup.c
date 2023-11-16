a138bd9be76623bee26361cae6eaf494cc3916cc
Reina Saito <resaito@c1r18s1.42tokyo.jp>
2023-11-16 17:51:44 +0900

    heredoc 大枠

--- src_resaito/redirect_dup.c
+++ src_resaito/redirect_dup.c
@@ -6,7 +6,7 @@
 /*   By: resaito <resaito@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:05:49 by resaito           #+#    #+#             */
-/*   Updated: 2023/11/16 17:36:01 by resaito          ###   ########.fr       */
+/*   Updated: 2023/11/16 17:49:53 by resaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,69 +70,66 @@ int	heredoc_exec(char *delimiter)
 			free(line);
 			break ;
 		}
-		// if (line != NULL)
-		// 	write(pipefd[1], line, ft_strlen(line));
-		// write(pipefd[1], "\n", 1);
-		printf("%s\n", line);
+		ft_putendl_fd(line, pipefd[1]);
 		free(line);
 	}
 	close(pipefd[1]);
 	return (pipefd[0]);
 }
 
-t_node *make_node(enum e_type node_type, char **args)
-{
-    t_node *node;
+// t_node *make_node(enum e_type node_type, char **args)
+// {
+//     t_node *node;
 
-    node = calloc(sizeof(t_node), 1);
-    node->type = node_type;
-    node->name = args[0];
-    node->args = args;
-    return node;
-}
+//     node = calloc(sizeof(t_node), 1);
+//     node->type = node_type;
+//     node->name = args[0];
+//     node->args = args;
+//     return node;
+// }
 
-t_redir *make_redir(enum e_type node_type, char **file)
-{
-	t_redir *redir;
+// t_redir *make_redir(enum e_type node_type, char **file)
+// {
+// 	t_redir *redir;
 
-	redir = malloc(sizeof(t_redir) * 1);
-	redir->file = file;
-	redir->type = node_type;
-	redir->next = NULL;
-	return redir;
-}
+// 	redir = malloc(sizeof(t_redir) * 1);
+// 	redir->file = file;
+// 	redir->type = node_type;
+// 	redir->next = NULL;
+// 	return redir;
+// }
 
-int main()
-{
-    t_node *ast;
-	t_redir *redir;
-	t_redir *redir2;
-    // char *args[] = {"/usr/bin/wc", "-l", NULL};
-    // char *args2[] = {"/bin/cat", NULL};
-    // char *args3[] = {"/usr/bin/grep", "hoge", NULL};
-    char *ls[] = {"/bin/ls", NULL};
-    char *cat[] = {"/bin/cat", NULL};
-    char *wc[] = {"/usr/bin/wc", "-l", NULL};
-    char *grep[] = {"/usr/bin/grep", "hoge", NULL};
-	char *file[] = {"hoge.txt", NULL};
-	char *file2[] = {"fuga.txt", NULL};
-	char *file3[] = {"piyo.txt", NULL};
-	char *eof[] = {"hoge", NULL};
+// int main()
+// {
+//     t_node *ast;
+// 	t_redir *redir;
+// 	t_redir *redir2;
+//     // char *args[] = {"/usr/bin/wc", "-l", NULL};
+//     // char *args2[] = {"/bin/cat", NULL};
+//     // char *args3[] = {"/usr/bin/grep", "hoge", NULL};
+//     char *ls[] = {"/bin/ls", NULL};
+//     char *cat[] = {"/bin/cat", NULL};
+//     char *wc[] = {"/usr/bin/wc", "-l", NULL};
+//     char *grep[] = {"/usr/bin/grep", "hoge", NULL};
+// 	char *file[] = {"hoge.txt", NULL};
+// 	char *file2[] = {"fuga.txt", NULL};
+// 	char *file3[] = {"piyo.txt", NULL};
+// 	char *eof[] = {"hoge", NULL};
 
-    // ast = make_node(N_PIPE, ls);
-    ast = make_node(N_COMMAND, cat);
-    // ast->right = make_node(N_PIPE, ls);
-    // ast->right = make_node(N_COMMAND, grep);
-    // ast->right->right = make_node(N_COMMAND, wc);
+//     // ast = make_node(N_PIPE, ls);
+//     ast = make_node(N_COMMAND, cat);
+//     // ast->right = make_node(N_PIPE, ls);
+//     // ast->right = make_node(N_COMMAND, grep);
+//     // ast->right->right = make_node(N_COMMAND, wc);
 
-	redir = make_redir(N_REDIR_HERE, eof);
-	// redir->next = make_redir(N_REDIR_IN, file3);
-	// redir->next->next = make_redir(N_REDIR_OUT, file2);
-	ast->redir = redir;
-	// redir->next = redir2;
-    heredoc_exec(ast->redir->file[0]);
-    // command_exec(args2, true);
-    // command_exec(args3, false);
-    // wait(NULL);
-    // wait(NULL);
-}
+// 	redir = make_redir(N_REDIR_HERE, eof);
+// 	// redir->next = make_redir(N_REDIR_IN, file3);
+// 	// redir->next->next = make_redir(N_REDIR_OUT, file2);
+// 	ast->redir = redir;
+// 	// redir->next = redir2;
+//     heredoc_exec(ast->redir->file[0]);
+//     // command_exec(args2, true);
+//     // command_exec(args3, false);
+//     // wait(NULL);
+//     // wait(NULL);
+// }
