0104c881ce6d3f9c59f0a9eaf6b7b034a5ffdb4d
fwatanab <fwatanab@student.42tokyo.jp>
2023-11-22 21:06:56 +0900

    ADD libft_utils.h

--- inc/expansion.h
+++ inc/expansion.h
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 03:53:40 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/11/21 19:27:06 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/11/22 20:52:12 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 # define EXPANSION_H
 
 # include "../libft/inc/libft.h"
+# include "../libft/inc/libft_utils.h"
 # include <stdio.h>
 # include <stdbool.h>
+# include <string.h>
 
 typedef struct s_parm
 {
 	char		*token;
 	char		*tmp;
-	char		*env_name;
+	char		*env_var;
 }	t_parm;
 
 char	*expand_parameter(char *token);
