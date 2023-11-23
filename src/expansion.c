2fe68a6bce6f8a1c0be5ac5317e086cdcf64e3fc
fwatanab <fwatanab@student.42tokyo.jp>
2023-11-23 20:11:56 +0900

    delete_quote関数実装 / 0 leaks / norm OK

--- src/expansion.c
+++ src/expansion.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 17:52:58 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/11/23 19:07:02 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/11/23 20:07:51 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,30 @@ char	*expand_parameter(char *token)
 	return (new_token);
 }
 
+char	*delete_quote(char	*token)
+{
+	char	*new_token;
+	size_t	i;
+	size_t	j;
+
+	if (token[0] != '\'' && token[0] != '"')
+		return (token);
+	new_token = (char *)malloc(sizeof(char) * (ft_strlen(token) - 1));
+	if (!new_token)
+		return (NULL);
+	i = 0;
+	j = 1;
+	while (token[j])
+	{
+		if (token[j] == '\'' || token[j] == '"')
+			break ;
+		new_token[i++] = token[j++];
+	}
+	new_token[i] = '\0';
+	free(token);
+	return (new_token);
+}
+
 void	expansion(char **array)
 {
 	char	*new_array;
@@ -43,6 +67,7 @@ void	expansion(char **array)
 	while (array[i])
 	{
 		array[i] = expand_parameter(array[i]);
+		array[i] = delete_quote(array[i]);
 		i++;
 	}
 }
