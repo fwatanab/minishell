cadda979684748b92767ba9c3b6f96ec428839fe
fwatanab <fwatanab@student.42tokyo.jp>
2023-11-29 20:36:38 +0900

    control C + control \ bash通りの挙動完

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 18:26:49 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/11/29 18:50:03 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/11/29 20:31:41 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ void	signal_handler(int sig)
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
-	if (sig == SIGQUIT)
-		rl_redisplay();
+	else if (sig == SIGQUIT)
+		;
 }
 
 void	signal_fork_handler(int sig)
 {
 	if (sig == SIGINT)
 		write(1, "\n", 1);
-	if (sig == SIGQUIT)
+	else if (sig == SIGQUIT)
 	{
 		write(1, "Quit: ", 6);
 		ft_putnbr_fd(sig, 1);
