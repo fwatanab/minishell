35f01aaa36f29aea55decd97d5ece8a6d9259ee9
fwatanab <fwatanab@student.42tokyo.jp>
2023-11-29 09:58:51 +0900

    control + C / 開業の位置を修正

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 18:26:49 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/11/28 20:52:12 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/11/29 09:57:04 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,23 @@ void	signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
+		write(1, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
-		printf("\n"MINISHELL);
+//		write(1, "\n"MINISHELL, 13);
 	}
 	if (sig == SIGQUIT)
-		;
+	{
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void	signal_fork_handler(int sig)
 {
 	if (sig == SIGINT)
-		printf("\n");
+		write(1, "\n", 1);
 	if (sig == SIGQUIT)
 		;
 }
-
-//void	signal_c_handler(int sig_num)
-//{
-//	printf("test\n");
-//	exit(0);
-//}
