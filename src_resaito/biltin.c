7a4d44a21c5fdf8c6cfe418665007ae2e2094c5b
resaito <resaito@student.42tokyo.jp>
2023-12-04 22:38:47 +0900

    biltinとcdの追加

---
+++ src_resaito/biltin.c
@@ -0,0 +1,39 @@
+#include "../inc/minishell.h"
+
+bool    is_biltin(t_node *node)
+{
+    if (ft_strcmp(node->args[0], "echo") == 0)
+        return (true);
+    else if (ft_strcmp(node->args[0], "cd") == 0)
+        return (true);
+    else if (ft_strcmp(node->args[0], "pwd") == 0)
+        return (true);
+    else if (ft_strcmp(node->args[0], "export") == 0)
+        return (true);
+    else if (ft_strcmp(node->args[0], "unset") == 0)
+        return (true);
+    else if (ft_strcmp(node->args[0], "env") == 0)
+        return (true);
+    else if (ft_strcmp(node->args[0], "exit") == 0)
+        return (true);
+    else
+        return (false);
+}
+
+void    exec_biltin(t_node *node, t_envval *envval)
+{
+    if (ft_strcmp(node->args[0], "echo") == 0)
+        envval->status = echo(node, envval->env);
+    else if (ft_strcmp(node->args[0], "cd") == 0)
+        envval->status = cd(node, envval->env);
+    else if (ft_strcmp(node->args[0], "pwd") == 0)
+        envval->status = pwd(node, envval->env);
+    else if (ft_strcmp(node->args[0], "export") == 0)
+        envval->status = export(node, envval->env);
+    else if (ft_strcmp(node->args[0], "unset") == 0)
+        envval->status = unset(node, envval->env);
+    else if (ft_strcmp(node->args[0], "env") == 0)
+        envval->status = env(node, envval->env);
+    else if (ft_strcmp(node->args[0], "exit") == 0)
+        envval->status = exit(node, envval->env);
+}
--- src_resaito/cd.c
+++ src_resaito/cd.c
@@ -14,41 +14,40 @@
 
 int	cd(t_node *node, t_env *env)
 {
-	char	*str;
-	t_env	*tmp;
+	int val;
 
-	if (ft_strncmp(node->args[1], "/", 1) == 0)
-		str = node->args[1];
+	if (node->args[1] == NULL)
+		val = chdir(getenv("HOME"));
 	else
+		val = chdir(node->args[1]);
+	if (val < 0)
 	{
-		tmp = env;
-		if (ft_strncmp(node->args[1], "./", 2) == 0)
-			str = ft_strjoin(getenv("PWD"), node->args[1] + 2);
-		else if (ft_strncmp(node->args[1], "..", 2) == 0)
-			str = ft_strndup(getenv("PWD"), ft_strrchr(getenv("PWD"), '/')
-					- getenv("PWD"));
+		perror("cd");
+		return (1);
 	}
 	return (0);
 }
 
-t_node	*make_node(enum e_type node_type, char **args)
-{
-	t_node	*node;
+// t_node	*make_node(enum e_type node_type, char **args)
+// {
+// 	t_node	*node;
 
-	node = calloc(sizeof(t_node), 1);
-	node->type = node_type;
-	node->name = args[0];
-	node->args = args;
-	return (node);
-}
+// 	node = calloc(sizeof(t_node), 1);
+// 	node->type = node_type;
+// 	node->name = args[0];
+// 	node->args = args;
+// 	return (node);
+// }
 
-int	main(int ac, char **av, char **envp)
-{
-	t_node	*node;
-	t_env	*env;
-	char	*cd_arg[] = {"cd", "../src", NULL};
+// int	main(int ac, char **av, char **envp)
+// {
+// 	t_node	*node;
+// 	t_env	*env;
+// 	char	*cd_arg[] = {"cd", NULL};
 
-	node = make_node(N_COMMAND, cd_arg);
-	env = new_envs(envp);
-	cd(node, env);
-}
+// 	node = make_node(N_COMMAND, cd_arg);
+// 	env = new_envs(envp);
+// 	cd(node, env);
+// 	free(node);
+// 	envs_free(env);
+// }
