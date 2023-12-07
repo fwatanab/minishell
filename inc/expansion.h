42f1f0de19ec326a1ec627746095997ea7a2fa34
fwatanab <fwatanab@student.42tokyo.jp>
2023-12-07 23:27:59 +0900

    expにenvを追加

--- inc/expansion.h
+++ inc/expansion.h
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 03:53:40 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/12/06 22:10:07 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/12/07 23:18:07 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ t_parm	*parameter_init(char *token);
 char	*check_parameter(t_parm *parm, char *token, t_envval *envval);
 int		check_question(t_parm *parm, char *token, t_envval *envval);
 char	*update_string_with_status(t_parm *parm, char *status_str);
-void check_brackets_balance(size_t *brackets, char **token, char *env_name, t_parm *parm);
+void	check_brackets_balance(size_t *brackets, char **token, char *env_name, t_parm *parm);
 
 #endif
