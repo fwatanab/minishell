6ee53f03f34d2680d69de08068f9a89b1fe36b6c
resaito <resaito@student.42tokyo.jp>
2023-12-07 17:27:31 +0900

    引数の変更

--- src_resaito/biltin.c
+++ src_resaito/biltin.c
@@ -23,7 +23,7 @@ bool    is_biltin(t_node *node)
 void    exec_biltin(t_node *node, t_envval *envval)
 {
     if (ft_strcmp(node->args[0], "echo") == 0)
-        envval->status = echo(node, envval);
+        envval->status = echo(node);
     else if (ft_strcmp(node->args[0], "cd") == 0)
         envval->status = cd(node, envval);
     else if (ft_strcmp(node->args[0], "pwd") == 0)
@@ -33,7 +33,7 @@ void    exec_biltin(t_node *node, t_envval *envval)
     else if (ft_strcmp(node->args[0], "unset") == 0)
         envval->status = unset(node, envval);
     else if (ft_strcmp(node->args[0], "env") == 0)
-        envval->status = env(node, envval);
+        envval->status = env(envval);
     else if (ft_strcmp(node->args[0], "exit") == 0)
         envval->status = exit(node, envval);
 }
