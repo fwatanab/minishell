82b09944f183dae6df15ecee8f85c892d8cf941d
Futa Watanabe <fwatanab@c1r18s3.42tokyo.jp>
2023-12-07 19:13:55 +0900

    add exit command

--- inc/builtins.h
+++ inc/builtins.h
@@ -6,15 +6,15 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 09:47:55 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/12/07 09:55:24 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/12/07 18:33:22 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-# include <stdlib.h>
+# include "minishell.h"
 
-void	execute_exit(const char *cmd);
+void	execute_exit(char **cmd, t_envval *envval);
 
 #endif
