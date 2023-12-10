a9bc594bbef69a0a1237ffed1710c0a6b6c5bba9
resaito <resaito@student.42tokyo.jp>
2023-12-10 15:26:20 +0900

    bad freeが出てくる問題を修正

--- libft/src/libft_utils/ft_realloc.c
+++ libft/src/libft_utils/ft_realloc.c
@@ -12,8 +12,8 @@ void	*ft_realloc(void *ptr, size_t new_size)
 		return (NULL);
 	}
 	if (!ptr)
-		return (malloc(new_size));
-	new_ptr = malloc(new_size);
+		return (ft_calloc(new_size, 1));
+	new_ptr = ft_calloc(new_size, 1);
 	if (!new_ptr)
 		return (NULL);
 	old_size = ft_strlen(ptr);
