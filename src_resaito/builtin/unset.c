4671f23934dbfb4937c892e0f64fe159253dfa9c
Reina Saito <resaito@c1r18s6.42tokyo.jp>
2023-12-11 14:25:44 +0900

    unsetの挙動を修正

--- src_resaito/builtin/unset.c
+++ src_resaito/builtin/unset.c
@@ -6,13 +6,16 @@
 /*   By: resaito <resaito@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 13:27:05 by resaito           #+#    #+#             */
-/*   Updated: 2023/12/11 12:39:16 by resaito          ###   ########.fr       */
+/*   Updated: 2023/12/11 14:24:24 by resaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 static void	remove_env_entry(t_env **env, t_env *prev, t_env *current);
+static bool	is_equal_in_args(char *args);
+static int	unset_error_print(char *args);
+static int	unset_single(char *name, t_envval *envval);
 
 int	unset(t_node *node, t_envval *envval)
 {
@@ -23,20 +26,9 @@ int	unset(t_node *node, t_envval *envval)
 	size = 1;
 	while (node->args[size] != NULL)
 	{
-		tmp = envval->env;
-		tmp_back = tmp;
-		while (ft_strcmp(node->args[size], tmp->key) != 0 && tmp->next)
-		{
-			tmp_back = tmp;
-			tmp = tmp->next;
-		}
-		if (tmp->next)
-			remove_env_entry(&(envval->env), tmp_back, tmp);
-		else if (ft_strcmp(node->args[size], tmp->key) == 0)
-			tmp_back->next = NULL;
-		else
-			return (1);
-		env_free(tmp);
+		if (is_equal_in_args(node->args[size]))
+			return (unset_error_print(node->args[size]));
+		unset_single(node->args[size], envval);
 		size++;
 	}
 	return (0);
@@ -48,6 +40,43 @@ static void	remove_env_entry(t_env **env, t_env *prev, t_env *current)
 		*env = current->next;
 	else
 		prev->next = current->next;
+}
+
+static bool	is_equal_in_args(char *args)
+{
+	if (ft_strchr(args, '=') == NULL)
+		return (false);
+	return (true);
+}
+
+static int	unset_error_print(char *args)
+{
+	ft_putstr_fd("minishell: unset: `", STDERR_FILENO);
+	ft_putstr_fd(args, STDERR_FILENO);
+	ft_putendl_fd("': not a valid identififer", STDERR_FILENO);
+	return (1);
+}
+
+static int	unset_single(char *name, t_envval *envval)
+{
+	t_env	*tmp;
+	t_env	*tmp_back;
+
+	tmp = envval->env;
+	tmp_back = tmp;
+	while (tmp && ft_strcmp(name, tmp->key) != 0 && tmp->next)
+	{
+		tmp_back = tmp;
+		tmp = tmp->next;
+	}
+	if (tmp->next)
+		remove_env_entry(&(envval->env), tmp_back, tmp);
+	else if (ft_strcmp(name, tmp->key) == 0)
+		tmp_back->next = NULL;
+	else
+		return (0);
+	env_free(tmp);
+	return (0);
 }
 
 // t_node	*make_node(enum e_type node_type, char **args)
@@ -68,7 +97,7 @@ static void	remove_env_entry(t_env **env, t_env *prev, t_env *current)
 //     t_envval *envval;
 //     char    **array;
 //     int     size = 0;
-// 	char	*unset_arg[] = {"export", "$PATH", NULL};
+// 	char	*unset_arg[] = {"unset", "PATH", NULL};
 
 // 	node = make_node(N_COMMAND, unset_arg);
 // 	envval = make_envval(new_envs(envp)); //make_envval.c make_env.c
