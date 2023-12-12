60beb4cd0de8871e2dd32b8354327ca79846db4b
Reina Saito <resaito@c1r18s5.42tokyo.jp>
2023-12-12 13:01:20 +0900

    getenvからft_getenvに変更

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: resaito <resaito@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 21:17:45 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/12/11 20:03:17 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:52:11 by resaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../inc/builtins.h"
 # include "../libft/inc/libft.h"
 # include "../libft/inc/libft_utils.h"
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stddef.h>
-# include <fcntl.h>
 # include "lexer_parser_utils.h"
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <signal.h>
-# include "../inc/builtins.h"
+# include <stddef.h>
+# include <stdio.h>
 
 # define MINISHELL "minishell $ "
 # define SPACE ' '
@@ -34,13 +34,13 @@
 # define REDIR_HIRE "<<"
 # define REDIR_APPEND ">>"
 
-extern int	sig_status;
+extern int				sig_status;
 
 typedef struct s_token_list
 {
 	char				*token;
 	struct s_token_list	*next;
-}	t_token_list;
+}						t_token_list;
 
 typedef enum e_type
 {
@@ -51,100 +51,102 @@ typedef enum e_type
 	N_REDIR_HERE,
 	N_REDIR_APPEND,
 	N_PIPE
-}	t_type;
+}						t_type;
 
 typedef struct s_redir
 {
-	enum e_type		type;
-	char			**file;
-	int				fd;
-	struct s_redir	*next;
-}	t_redir;
+	enum e_type			type;
+	char				**file;
+	int					fd;
+	struct s_redir		*next;
+}						t_redir;
 
 typedef struct s_node
 {
-	enum e_type		type;
-	char			*name;
-	char			**args;
-	struct s_redir	*redir;
-	struct s_node	*left;
-	struct s_node	*right;
-}	t_node;
+	enum e_type			type;
+	char				*name;
+	char				**args;
+	struct s_redir		*redir;
+	struct s_node		*left;
+	struct s_node		*right;
+}						t_node;
 
 typedef struct s_env
 {
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}	t_env;
+	char				*key;
+	char				*value;
+	struct s_env		*next;
+}						t_env;
 
 typedef struct s_envval
 {
-	t_env	*env;
-	int		status;
-}	t_envval;
+	t_env				*env;
+	int					status;
+}						t_envval;
 
 //lexer parser
-t_token_list	*tokenize(const char *str);
-void			check_token(t_token_list *list);
-t_node			*parser_start(t_token_list **list);
-t_node			*parser(t_node *node, t_token_list **list, t_parse_check *key);
-t_redir			*redir_parse(t_node *node, t_redir *redir, \
-		t_token_list **list, char *token);
+t_token_list			*tokenize(const char *str);
+void					check_token(t_token_list *list);
+t_node					*parser_start(t_token_list **list, t_env *env);
+t_node					*parser(t_node *node, t_token_list **list,
+							t_parse_check *key, t_env *env);
+t_redir					*redir_parse(t_node *node, t_redir *redir,
+							t_token_list **list, char *token);
 
 //expansion
-void			check_exp(t_node *node, t_envval *envval);
+void					check_exp(t_node *node, t_envval *envval);
 
 //signal
-void			signal_handler(int sig);
-void			signal_fork_handler(int sig);
-void			check_status(t_envval *envval);
+void					signal_handler(int sig);
+void					signal_fork_handler(int sig);
+void					check_status(t_envval *envval);
 
 //free
-void			list_free(t_token_list **list);
-void			str_array_free(char **array);
-void			node_free(t_node *node);
-void			redir_free(t_redir *redir);
+void					list_free(t_token_list **list);
+void					str_array_free(char **array);
+void					node_free(t_node *node);
+void					redir_free(t_redir *redir);
 
 //error
-void			malloc_error(void);
+void					malloc_error(void);
 
 //resaito search_path
-char			*search_path(const char *filename);
+char					*search_path(const char *filename, t_env *env);
 
 // exec
-void			execution(t_node *node, bool is_exec_pipe, t_envval *envval);
-void			ft_execution(t_node *node, t_envval *envval);
-int				child_process(t_node *node, bool has_pipe, t_envval *envval,
-					int pipefd[2]);
-void			parent_process(bool has_pipe, int pipefd[2]);
+void					execution(t_node *node, bool is_exec_pipe,
+							t_envval *envval);
+void					ft_execution(t_node *node, t_envval *envval);
+int						child_process(t_node *node, bool has_pipe,
+							t_envval *envval, int pipefd[2]);
+void					parent_process(bool has_pipe, int pipefd[2]);
 
 // redir
-int				redir_dup(t_node *node);
-int				indirect_exec(t_node *node);
-int				heredoc_exec(t_redir *redir, t_envval *envval);
-int				input_redir(t_node *node, t_envval *envval);
-bool			is_type_heredoc(t_redir *redir);
-bool			is_type_indirect(t_redir *redir);
-int				dup_2_stdin(t_node *node);
+int						redir_dup(t_node *node);
+int						indirect_exec(t_node *node);
+int						heredoc_exec(t_redir *redir, t_envval *envval);
+int						input_redir(t_node *node, t_envval *envval);
+bool					is_type_heredoc(t_redir *redir);
+bool					is_type_indirect(t_redir *redir);
+int						dup_2_stdin(t_node *node);
 
 // env
-t_env			*new_envs(char **envp);
-void			envadd_back(t_env **env, t_env *new);
-t_env			*new_env(char *envp);
-char			**make_env_strs(t_env *env);
-size_t			ft_list_size(t_env *env);
-void			envs_free(t_env *env);
-void			envs_str_free(t_env *env, char **str);
-void			env_free(t_env *env);
-t_envval		*make_envval(t_env *env);
+t_env					*new_envs(char **envp);
+void					envadd_back(t_env **env, t_env *new);
+t_env					*new_env(char *envp);
+char					**make_env_strs(t_env *env);
+size_t					ft_list_size(t_env *env);
+void					envs_free(t_env *env);
+void					envs_str_free(t_env *env, char **str);
+void					env_free(t_env *env);
+t_envval				*make_envval(t_env *env);
 
 // utils
-void			*ft_xmalloc(size_t size);
-void			ft_perror(char *str);
-int				ft_pipe(int pipefd[2]);
-int				print_error(char *command, char *error, int status);
-bool			is_single_command(t_node *node);
-int				get_exit_code(int n);
+void					*ft_xmalloc(size_t size);
+void					ft_perror(char *str);
+int						ft_pipe(int pipefd[2]);
+int						print_error(char *command, char *error, int status);
+bool					is_single_command(t_node *node);
+int						get_exit_code(int n);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: resaito <resaito@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 21:17:52 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/12/12 11:11:11 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:09:42 by resaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	minishell(char *line, t_envval *envval)
 	list = tokenize(line);
 	tmp = list;
 	check_token(list);
-	node = parser_start(&list);
+	node = parser_start(&list, envval->env);
 	check_exp(node, envval);
 	list_free(&tmp);
 	ft_execution(node, envval);
