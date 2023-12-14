d9dc16e7b5954ebb05735fe57dfcc81d3ed9bcd9
fwatanab <fwatanab@student.42tokyo.jp>
2023-12-14 16:48:58 +0900

    norm clear

--- inc/expansion.h
+++ inc/expansion.h
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 03:53:40 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/12/14 16:29:03 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/12/14 16:48:36 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ typedef struct s_parm
 	size_t		len;
 }	t_parm;
 
-
 typedef struct s_quote_status
 {
 	bool	d_quote;
@@ -35,16 +34,16 @@ typedef struct s_quote_status
 }	t_quote_status;
 
 //init
-t_parm	*parameter_init(char *token);
+t_parm			*parameter_init(char *token);
 
-char	*check_command(char *str, t_envval *envval);
-char	*expand_parameter(char *token, t_envval *envval);
-char	*check_parameter(t_parm *parm, t_envval *envval);
-int		check_question(t_parm *parm, t_envval *envval);
-char	*ft_getenv(char *str, t_env *env);
-char	*update_string_with_status(t_parm *parm, char *status_str);
+char			*check_command(char *str, t_envval *envval);
+char			*expand_parameter(char *token, t_envval *envval);
+char			*check_parameter(t_parm *parm, t_envval *envval);
+int				check_question(t_parm *parm, t_envval *envval);
+char			*ft_getenv(char *str, t_env *env);
+char			*update_string_with_status(t_parm *parm, char *status_str);
 t_quote_status	*quote_status_init(char *token);
-void	check_brackets_balance(size_t *brackets, char **token, \
+void			check_brackets_balance(size_t *brackets, char **token, \
 		char *env_name, t_parm *parm);
 
 #endif
