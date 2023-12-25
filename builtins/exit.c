ce0066b2ed95fc66cf75e5210a9859e249868b65
Futa Watanabe <fwatanab@c1r18s8.42tokyo.jp>
2023-12-25 16:41:58 +0900

    複数の引数の時の挙動修正

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 09:46:50 by fwatanab          #+#    #+#             */
-/*   Updated: 2023/12/11 17:22:53 by fwatanab         ###   ########.fr       */
+/*   Updated: 2023/12/24 18:51:34 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,26 @@ void	ft_exit(t_node *node, t_envval *envval)
 	len = 0;
 	while (node->args[len])
 		len++;
-	if (len <= 2 && ft_strcmp(node->args[0], "exit") == 0)
+	if (ft_strcmp(node->args[0], "exit") == 0)
 	{
-		if (node->args[1] != NULL && is_numeric_str(node->args[1]))
-			status = ft_atoi(node->args[1]);
-		else if (node->args[1] != NULL)
+		write(1, "exit\n", 5);
+		if (node->args[1] && is_numeric_str(node->args[1]))
 		{
-			write(2, "exit: numeric argument required\n", 33);
+			if (2 < len)
+			{
+				write(2, "minishell: exit: too many arguments\n", 36);
+				status = 1;
+			}
+			else
+				status = ft_atoi(node->args[1]);
+		}
+		else if (node->args[1])
+		{
+			write(2, "minishell: exit: numeric argument required\n", 43);
 			status = 255;
 		}
 		else
 			status = envval->status;
-		write(1, "exit\n", 5);
 		node_free(node);
 		exit(status);
 	}
